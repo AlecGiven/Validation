d0b0a088ed738f524d3aaa2504aca488d56737a2
AlecGiven <78773178+AlecGiven@users.noreply.github.com>
2021-04-11 23:53:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const password = loginForm.password.value;
+
+    if (password === "coolStudent123") {
+   <html>    
+  <body> 
+   
+   <input type="submit" onClick="myFunction()"/> 
+   <script>
+     function myFunction() {
+       window.location.href="<html>    
+  <body> 
+   
+   <input type="submit" onClick="myFunction()"/> 
+   <script>
+     function myFunction() {
+       window.location.href="https://codepen.io/AlecGiven/pen/YzNeeGQ";  
+
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
